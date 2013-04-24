89ef79d5e410e9449621e1cc84efeb01e318a0c8
John R. Sippy <john.r.sippy@gmail.com>
2013-04-24 13:56:25 -0500

    Added a safety to prevent voting for a game that we already own. Added "Vote received" or "Vote not possible" messaging to the user. Cleaned up / added comments for clarification.

--- lib/Controller/SierraBravo/xboxV2/GameLibrarian.php
+++ lib/Controller/SierraBravo/xboxV2/GameLibrarian.php
@@ -15,6 +15,7 @@ class Controller_SierraBravo_xboxV2_GameLibrarian extends Controller {
 		$this->key=$this->api->getConfig('SierraBravo/xboxV2/GameLibrarian/key');
 		$this->debug=$this->api->getConfig('SierraBravo/xboxV2/GameLibrarian/debug');
 	}
+	// Sets up a request action for the model to execute
 	function addRequest($type){
 		return $this
 			->add('Model_SierraBravo_xboxV2_Soap')
@@ -22,11 +23,12 @@ class Controller_SierraBravo_xboxV2_GameLibrarian extends Controller {
 			->addHook('request-complete',array($this,'requestComplete'))
 			;
 	}
+	// Hook (or callback) from the model
+	//  This function is executed after successful request
 	function requestComplete($transaction,$response){
-		// This function is executed after successful request
 		$this->hook('request-complete',array($transaction,$response));
 	}
-
+	// Returns our list of games from the Model, formatted as an associative array
 	function getGames(){
 		$r=$this->addRequest('getGames')->process();
 		$data=array();
@@ -43,6 +45,9 @@ class Controller_SierraBravo_xboxV2_GameLibrarian extends Controller {
 		$this->data=$data;
 		return $this->data;
 	}
+	// Tells the Model to add a game to our list
+	//  Checks our game list first to make sure the game is not already listed
+	//  Returns TRUE on success signal from the Model, and FALSE on failure
 	function addGame($title){
 		if( ($title!=undefined) && ($title!=null) ){
 			$data=$this->getGames();
@@ -64,16 +69,40 @@ class Controller_SierraBravo_xboxV2_GameLibrarian extends Controller {
 		}
 		return false;
 	}
+	// Tell the model to add a vote to our game's vote count
+	//  Returns TRUE on success, FALSE on failure
 	function addVote($gameId){
-		return $this->addRequest('addVote')
-			->set('id',$gameId)
-			->process();
+		if($this->ownIt($gameId) == false){
+			return $this->addRequest('addVote')
+				->set('id',$gameId)
+				->process();
+		}
+		return false;
 	}
+	// Get our games list from the Model and return:
+	//  TRUE if we already own it
+	//  FALSE if we do not
+	function ownIt($gameId){
+		if( ($gameId!=undefined) && ($gameId!=null) ){
+			$data=$this->getGames();
+			foreach($data as $item){
+				if( strcmp($gameId, $item['gameId']) == 0 ){
+					if( strcmp($item['ownIt'],"Yes") == 0 ){
+						return true;
+					}
+					break;
+				}
+			}
+		}
+		return false;
+	}
+	// Tell the Modle to mark a game as 'owned'
 	function setGotIt($gameId){
 		return $this->addRequest('setGotIt')
 			->set('id',$gameId)
 			->process();
 	}
+	// Tell the Model to clear our games list
 	function clearGames(){
 		//TODO - Add a Confirmation Dialog here
 		return $this->addRequest('clearGames')->process();
--- lib/Page/GameLibrarian.php
+++ lib/Page/GameLibrarian.php
@@ -53,18 +53,25 @@ class Page_GameLibrarian extends Page {
 
 		// *** Button Click actions ***
 		
+		// These actions are performed after the "Vote" button is clicked
 		if(isset($_GET['Vote'])){
 			$gId=$_GET['Vote'];
 			$gameId=$data[$gId]['gameId'];
 			$success=$this->cm->addVote($gameId);
+			$message="";
 			if($success){
-				$voteManaged=$cookie->voted();
-				$this->js()
-					->_selector('.reloadGamesPage')->trigger('reloadGamesPage')
-					->execute()
-				;
+				$cookie->voted();
+				$message="Thank you, we have recorded your vote.";
+			} else {
+				$message="Sorry, you can not vote for this game at this time.";
 			}
+			$this->js()
+				->_selector('.reloadGamesPage')->trigger('reloadGamesPage')
+				->univ()->alert($message)
+				->execute()
+			;
 		}
+		// These actions are performed after the Admin's "Got It" button is clicked
 		if(isset($_GET['GotIt'])){
 			$gId=$_GET['GotIt'];
 			$gameId=$data[$gId]['gameId'];
@@ -76,6 +83,7 @@ class Page_GameLibrarian extends Page {
 				;
 			}
 		}
+		// These actions are performed after the page is reloaded in 'clear games mode' by the 'page_clearGames' function
 		if(isset($_GET['page'])){
 			if($_GET['page']=='clearGames'){
 				$success=$this->cm->clearGames();
@@ -88,6 +96,7 @@ class Page_GameLibrarian extends Page {
 			}
 		}
 	}
+	// This function is run after the Admin's "Clear Games" button is pressed
 	function page_clearGames(){
 		$r=$this->add('Controller_SierraBravo_xboxV2_GameLibrarian')->clearGames();
 		header('Location: '.$this->api->pm->base_path."?page=admin");
